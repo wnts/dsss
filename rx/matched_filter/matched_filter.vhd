c27294aa21f852b596e3584335b727e26bef73bb
Wouter Nuyts <wouter.nuyts@student.thomasmore.be>
2017-05-06 18:22:09 +0200

    Matched filter + testbench

--- rx/matched_filter/matched_filter.vhd
+++ rx/matched_filter/matched_filter.vhd
@@ -3,29 +3,52 @@ use ieee.std_logic_1164.all;
 
 entity matched_filter is
 	port(clk		: in std_logic;	     		 
-	     reset		: in std_logic;		 
-		 sdi		: in std_logic;
+		 reset		: in std_logic;
+		 ce			: in std_logic;
+		 sdi_spread	: in std_logic;
 		 sel		: in std_logic_vector(1 downto 0);
 		 seq_det	: out std_logic);
 end matched_filter;
 
 architecture behave of matched_filter is	
-		signal reg_cur, reg_next : std_logic_vector(N-1 downto 0) := (others => '0');
+	constant no_ptrn   : std_logic_vector(30 downto 0) := (others => '0');
+	constant ml1_ptrn  : std_logic_vector(30 downto 0) := "1101110011111100011011101010001";
+	constant ml2_ptrn  : std_logic_vector(30 downto 0) := "1001101111101000100101011000011";
+	constant gold_ptrn : std_logic_vector(30 downto 0) := ml1_ptrn xor ml2_ptrn;
+	signal pn_ptrn 	   : std_logic_vector(30 downto 0);
+	signal present_reg, next_reg : std_logic_vector(30 downto 0) := (others => '0');
 begin
 	syn_matched_filter : process(clk)
 	begin
 		if rising_edge(clk) then
 			if reset = '1' then
-				reg_cur <= (others => '0');
-			else
-				reg_cur <= reg_next;
+				present_reg <= (others => '1');
+			elsif ce = '1' then
+				present_reg <= next_reg;
 			end if;
-		end if;
+		end if;	
 	end process syn_matched_filter;
-	
-	com_matched_filter : process(reg_next, reg_cur, sdi)
+
+	com_matched_filter : process(sdi_spread, present_reg, sel, pn_ptrn)
 	begin
-		reg_next <= reg_cur(N-2 downto 0) & sdi;
-		sdo <= reg_cur(N-1);
+		next_reg <= sdi_spread & present_reg(30 downto 1);
+		-- output	
+		case sel is
+			when "00" => 
+				pn_ptrn <= no_ptrn;
+			when "01" => 
+				pn_ptrn <= ml1_ptrn;
+			when "10" => 
+				pn_ptrn <= ml2_ptrn;
+			when "11" => 
+				pn_ptrn <= gold_ptrn;
+			when others =>
+				pn_ptrn <= no_ptrn;
+		end case;
+		if  present_reg = pn_ptrn then
+			seq_det <= '1';
+		else
+			seq_det <= '0';
+		end if;
 	end process com_matched_filter;
 end behave;
---
+++ rx/matched_filter/matched_filter_tb.vhd
@@ -0,0 +1,158 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity matched_filter_tb is
+end matched_filter_tb;
+
+architecture structural of matched_filter_tb is 
+	-- component declaraties
+	component matched_filter is
+		port(clk		: in std_logic;	     		 
+			 reset		: in std_logic;
+			 ce			: in std_logic;
+			 sdi_spread	: in std_logic;
+			 sel		: in std_logic_vector(1 downto 0);
+			 seq_det	: out std_logic);
+	end component;
+
+
+for uut : matched_filter use entity work.matched_filter(behave);
+ 
+	constant period		: time := 100 ns;
+	constant delay		: time :=  10 ns;
+	signal end_of_sim	: boolean := false;
+
+	constant no_ptrn   : std_logic_vector(30 downto 0) := (others => '0');
+	constant ml1_ptrn  : std_logic_vector(30 downto 0) := "1101110011111100011011101010001";
+	constant ml2_ptrn  : std_logic_vector(30 downto 0) := "1001101111101000100101011000011";
+	constant gold_ptrn : std_logic_vector(30 downto 0) := ml1_ptrn xor ml2_ptrn;
+
+	signal clk			: std_logic;
+	signal reset		: std_logic;
+	signal ce			: std_logic;
+	signal sdi_spread	: std_logic;
+	signal seq_det   	: std_logic;
+	signal sel			: std_logic_vector(1 downto 0);
+begin
+	uut: matched_filter
+		port map(clk	 	 => clk,      
+				 reset		 => reset,
+				 ce		 	 => ce,
+				 sel		 => sel,
+				 sdi_spread	 => sdi_spread,
+				 seq_det	 => seq_det);
+	clock : process
+	begin 
+		clk <= '0';
+		wait for period/2;
+		loop
+			clk <= '0';
+			wait for period/2;
+			clk <= '1';
+			wait for period/2;
+			exit when end_of_sim;
+		end loop;
+		wait;
+	end process clock;
+		
+	-- ************************************************************************
+	--  Matched filter testbench
+	-- *************************************************************************
+	tb : process
+		procedure tbvector(constant stimvect : in std_logic_vector(4 downto 0))is
+		begin	  
+			reset <= stimvect(4);
+			ce <= stimvect(3);
+			sdi_spread <= stimvect(2);
+			sel <= stimvect(1 downto 0);
+		   wait for period;
+		end tbvector;
+	begin		
+		-- reset
+		tbvector("10111");
+		tbvector("10111");
+		-- eerst wat rommel aanleggen...		
+		for i in 0 to 32 loop
+			tbvector("01111");
+		end loop;
+		-- even tussenpauze om te controleren of seq_det laag blijft 
+		tbvector("01111");		
+		tbvector("01111");		
+
+		-- ***************
+		-- no_ptrn test 
+		-- ***************
+		-- ce even afzetten zodat we in waveform overgang kunnen zien
+		tbvector("00100");
+		tbvector("00100");
+		-- no_ptrn code aanleggen en selecteren via sel -> dit moet een seq_det puls geven na de for loop
+		for i in 0 to 30 loop
+			tbvector("01" & no_ptrn(i) & "00");
+		end loop;
+		-- even tussenpauze om te controleren of seq_det terug laag wordt 
+		tbvector("01100");		
+		tbvector("01100");		
+
+		-- ***************
+		-- ml1_ptrn test 
+		-- ***************
+		-- ce even afzetten zodat we in waveform overgang kunnen zien
+		tbvector("00011");
+		tbvector("00011");
+		-- ml1_ptrn code aanleggen en selecteren via sel -> dit moet een seq_det puls geven na de for loop
+		for i in 0 to 30 loop
+			tbvector("01" & ml1_ptrn(i) & "01");
+		end loop;
+		-- even tussenpauze om te controleren of seq_det terug laag wordt 
+		tbvector("01001");		
+		tbvector("01001");		
+
+		-- ***************
+		-- ml2_ptrn test 
+		-- ***************
+		-- ce even afzetten zodat we in waveform overgang kunnen zien
+		tbvector("00010");
+		tbvector("00010");
+		-- ml1_ptrn code aanleggen en selecteren via sel -> dit moet een seq_det puls geven na de for loop
+		for i in 0 to 30 loop
+			tbvector("01" & ml2_ptrn(i) & "10");
+		end loop;
+		-- even tussenpauze om te controleren of seq_det terug laag wordt 
+		tbvector("01110");		
+		tbvector("01110");		
+
+
+		-- ***************
+		-- gold_ptrn test 
+		-- ***************
+		-- ce even afzetten zodat we in waveform overgang kunnen zien
+		tbvector("00011");
+		tbvector("00011");		
+		-- gold_ptrn code aanleggen en selecteren via sel -> dit moet een seq_det puls geven na de for loop
+		for i in 0 to 30 loop
+			tbvector("01" & gold_ptrn(i) & "11");
+		end loop;
+		-- even tussenpauze om te controleren of seq_det terug laag wordt 
+		tbvector("01111");		
+		tbvector("01111");		
+
+
+		-- ce even afzetten zodat we in waveform overgang kunnen zien
+		tbvector("00011");
+		tbvector("00011");
+		-- ml1_ptrn code aanleggen terwijl we pn_ml2 selecteren via sel -> dit mag geen seq_det puls geven na de for loop
+		for i in 0 to 30 loop
+			tbvector("01" & ml1_ptrn(i) & "10");
+		end loop;
+		-- even tussenpauze om te controleren dat seq_det laag is en blijft
+		tbvector("01111");
+
+		end_of_sim <= true;
+		wait;
+	end process;
+end;
+
+	
+	
